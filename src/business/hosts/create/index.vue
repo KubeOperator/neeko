7b29b82cb5834956cb1ac640614f494c1b7af8cd
chenyang <907693148@qq.com>
2021-03-23 19:08:11 +0800

    feat(host): 主机模块

--- src/business/hosts/create/index.vue
+++ src/business/hosts/create/index.vue
@@ -5,43 +5,50 @@
       <el-col :span="16">
         <div class="grid-content bg-purple-light">
           <el-form ref="form" :model="form" label-width="80px">
-            <el-form-item :label="$t('commons.table.name')">
+            <el-form-item :label="$t('commons.table.name')" required>
               <el-input v-model="form.name"></el-input>
             </el-form-item>
-            <el-form-item label="IP">
+            <el-form-item label="IP" required>
               <el-input v-model="form.ip"></el-input>
             </el-form-item>
-            <el-form-item :label="$t('host.port')">
+            <el-form-item :label="$t('host.port')" required>
               <el-input type="number" min="0" max="65535" placeholder="22" v-model="form.port"></el-input>
             </el-form-item>
 
-            <el-form-item label="凭据">
-              <el-radio-group v-model="form.credentialType">
-                <el-radio label="exists">已存在的凭据</el-radio>
-                <el-radio label="new">新建凭据</el-radio>
+            <el-form-item :label="$t('credential.credential')" required>
+              <el-radio-group v-model="credentialType">
+                <el-radio label="exists">{{$t('host.exists_credential')}}</el-radio>
+                <el-radio label="new">{{$t('host.new_credential')}}</el-radio>
               </el-radio-group>
             </el-form-item>
-            <el-form-item v-if="form.credentialType==='exists'">
-              <el-select v-model="form.region" placeholder="请选择凭据">
-                <el-option label="凭据1" value="shanghai"></el-option>
-                <el-option label="凭据2" value="beijing"></el-option>
+            <el-form-item v-if="credentialType==='exists'" :label="$t('host.credential_name')" required>
+              <el-select v-model="form.credentialName" placeholder="请选择凭据">
               </el-select>
             </el-form-item>
 
-            <span v-if="form.credentialType==='new'">
-               <el-form-item label="凭据名称">
+            <span v-if="credentialType==='new'">
+               <el-form-item :label="$t('host.credential_name')" required>
                  <el-input v-model="form.credential.name"></el-input>
                </el-form-item>
-               <el-form-item label="用户名">
+               <el-form-item :label="$t('credential.username')" required>
                  <el-input v-model="form.credential.username"></el-input>
                </el-form-item>
-               <el-form-item label="密码">
+              <el-form-item :label="$t('credential.credential')" required>
+              <el-radio-group v-model="form.credential.type">
+                <el-radio label="password">{{$t('credential.password')}}</el-radio>
+                <el-radio label="privateKey">{{$t('credential.privateKey')}}</el-radio>
+              </el-radio-group>
+              </el-form-item>
+               <el-form-item v-if="form.credential.type==='password'" :label="$t('credential.password')" required>
                  <el-input type="password" v-model="form.credential.password"></el-input>
+               </el-form-item>
+               <el-form-item v-if="form.credential.type==='privateKey'" :label="$t('credential.privateKey')" required>
+                 <el-input type="textarea" v-model="form.credential.privateKey"></el-input>
                </el-form-item>
             </span>
             <el-form-item>
-              <el-button type="primary" @click="onSubmit">立即创建</el-button>
               <el-button @click="onCancel()">取消</el-button>
+              <el-button type="primary" @click="onSubmit">创建</el-button>
             </el-form-item>
           </el-form>
         </div>
@@ -53,33 +60,39 @@
 
 <script>
   import LayoutContent from "@/components/layout/LayoutContent";
+  import {createHost} from "@/api/hosts"
+
 
   export default {
     name: "HostCreate",
     components: {LayoutContent},
     data() {
       return {
+        credentialType: "exists",
         form: {
-          name: '',
-          region: '',
-          date1: '',
-          date2: '',
-          delivery: false,
-          type: [],
-          resource: '',
-          desc: '',
-          credentialType: "exists",
+          name: "",
+          ip: "",
+          port: 22,
+          credentialName: "",
           credential: {
             username: "",
             password: "",
-            name: ""
+            name: "",
+            type: "password",
+            privateKey: ""
           }
         }
       }
     },
     methods: {
       onSubmit() {
-        console.log('submit!');
+        createHost(this.form).then(() => {
+          this.$message({
+            type: 'success',
+            message: `创建成功`
+          });
+          this.$router.push({name: "HostList"})
+        })
       },
       onCancel() {
         this.$router.push({name: "HostList"})
