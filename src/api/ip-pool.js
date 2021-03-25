506e398345a4e8dba7eda2f88cda9a3931c3f8d3
wangzhengkun <zhengkun@fit2cloud.com>
2021-03-25 15:49:43 +0800

    feat(ip-pool): 增加IP-Pool搜索功能

--- src/api/ip-pool.js
+++ src/api/ip-pool.js
@@ -14,6 +14,11 @@ export function createIpPool (data) {
   return post(ipPoolUrl, data)
 }
 
+export function searchIpPool(currentPage, pageSize, conditions) {
+  return post(`${ipPoolUrl}/search?pageNum=${currentPage}&pageSize=${pageSize}`, conditions)
+
+}
+
 export function listIps (ipPoolName, page, size) {
   return get(`ippools/${ipPoolName}/ips?pageNum=${page}&pageSize=${size}`)
 }
@@ -30,6 +35,6 @@ export function syncIp (ipPoolName) {
   return patch(`${ipPoolUrl}/${ipPoolName}/ips/sync`)
 }
 
-export function searchIp (currentPage, pageSize, ipPoolName, conditions,) {
+export function searchIp (currentPage, pageSize, ipPoolName, conditions) {
   return post(`${ipPoolUrl}/${ipPoolName}/ips/search?pageNum=${currentPage}&pageSize=${pageSize}`, conditions)
 }
