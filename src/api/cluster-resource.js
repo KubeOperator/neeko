699aa1c0761e7a36069141771cfbe95fce592ae7
wangzhengkun <zhengkun@fit2cloud.com>
2021-04-06 16:53:39 +0800

    feat(cluster-resource): 增加cluster-resource 读取和添加

---
+++ src/api/cluster-resource.js
@@ -0,0 +1,13 @@
+import {get, post} from "@/plugins/request"
+
+const clusterResourceUrl = (cluster_name) => {
+  return `/cluster/${cluster_name}/resources`
+}
+
+export function listClusterResources (clusterName,resourceType, currentPage, pageSize) {
+  return get(`${clusterResourceUrl(clusterName)}?pageNum=${currentPage}&pageSize=${pageSize}&resourceType=${resourceType}`)
+}
+
+export function createClusterResource (clusterName,data) {
+  return post(`${clusterResourceUrl(clusterName)}`, data)
+}
