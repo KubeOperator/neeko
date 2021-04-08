bcaeb1d64277b8bd42978fa48316179f5c9d7748
xiaohu <564134281@qq.com>
2021-04-08 18:56:00 +0800

    feat(messageApi): 更新message API

--- src/api/message.js
+++ src/api/message.js
@@ -8,14 +8,18 @@ export function getMessageReceicver() {
   return get("message/receiver")
 }
 
-export function updateMessageReceicver(data) {
-  return post("message/receiver", data)
-}
-
 export function getMessageOfSubscribe() {
   return get("message/subscribe")
 }
 
 export function updateMessageOfSubscribe(data) {
   return post("message/subscribe", data)
+}
+
+export function updateMessageReceicver(data) {
+  return post("message/receiver", data)
+}
+
+export function updateMessageStatus(data) {
+  return post("message/mail/batch", data)
 }
