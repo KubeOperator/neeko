3a9a2a60f697ae222fe1eb8bb9d4d6de7a2107e5
ssongliu <songlius11@163.com>
2021-04-15 19:07:45 +0800

    fix(manifest): 冲突解决

--- src/api/manifest.js
+++ src/api/manifest.js
@@ -6,10 +6,6 @@ export function manifestGroup() {
   return get("manifests/group")
 }
 
-export function changeStatus(versionName, version) {
-  return patch(`manifests/${versionName}`, version)
-}
-
 export function listActive() {
   return get(baseUrl + '/active');
 }
