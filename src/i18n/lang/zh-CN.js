33c90fb1b09d34aa66aca3f53dd0d2cc0aa44dc7
xiaohu <564134281@qq.com>
2021-04-25 17:19:49 +0800

    feat(login):  修改默认登录的title

--- src/i18n/lang/zh-CN.js
+++ src/i18n/lang/zh-CN.js
@@ -131,7 +131,7 @@ const message = {
   login: {
     username: "用户名",
     password: "密码",
-    title: "登录 FIT2CLOUD",
+    title: "登录 KubeOperator",
     welcome: "欢迎回来，请输入用户名和密码登录",
     expires: "认证信息已过期，请重新登录"
   },
