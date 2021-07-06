4b87d9dac17938e31c253ff0b93fba9b48aac10d
ssongliu <songlius11@163.com>
2021-07-06 17:26:51 +0800

    fix(istio): arm或者混合架构不显示 istio 菜单

--- src/business/clusters/detail/index.vue
+++ src/business/clusters/detail/index.vue
@@ -1,31 +1,33 @@
 <template>
   <layout-content :header="$t('cluster.cluster_detail')" :back-to="{ name: 'ClusterList' }">
-    <el-menu class="menuClass" @select="search" router :default-active="$route.path" mode="horizontal">
-      <el-menu-item :index="'/clusters/detail/'+project+'/'+name+'/overview'">{{$t('cluster.detail.tag.overview')}}</el-menu-item>
-      <el-menu-item :index="'/clusters/detail/'+project+'/'+name+'/node'">{{$t('cluster.detail.tag.node')}}</el-menu-item>
-      <el-menu-item :index="'/clusters/detail/'+project+'/'+name+'/namespace'">{{$t('cluster.detail.tag.namespace')}}</el-menu-item>
-      <el-menu-item :index="'/clusters/detail/'+project+'/'+name+'/storage'">{{$t('cluster.detail.tag.storage')}}</el-menu-item>
-      <el-menu-item :index="'/clusters/detail/'+project+'/'+name+'/event'">{{$t('cluster.detail.tag.event')}}</el-menu-item>
-      <el-menu-item v-if="isPrometheusOn" :index="'/clusters/detail/'+project+'/'+name+'/monitor'">
-        {{$t('cluster.detail.tag.monitor')}}
-      </el-menu-item>
-      <el-menu-item v-if="isLoggingOn" :index="'/clusters/detail/'+project+'/'+name+'/logging'">{{$t('cluster.detail.tag.log')}}
-      </el-menu-item>
-      <el-menu-item v-if="isLokiOn" :index="'/clusters/detail/'+project+'/'+name+'/loki'">{{$t('cluster.detail.tag.log')}}
-      </el-menu-item>
-      <el-menu-item :index="'/clusters/detail/'+project+'/'+name+'/tool'">{{$t('cluster.detail.tag.tool')}}</el-menu-item>
-      <el-menu-item :index="'/clusters/detail/'+project+'/'+name+'/istio'">Istio</el-menu-item>
-      <el-menu-item :index="'/clusters/detail/'+project+'/'+name+'/backup'">{{$t('cluster.detail.tag.backup')}}</el-menu-item>
-      <el-menu-item v-if="hasLicense" :index="'/clusters/detail/'+project+'/'+name+'/f5'">F5</el-menu-item>
-      <el-menu-item :index="'/clusters/detail/'+project+'/'+name+'/security'">
-        {{$t('cluster.detail.tag.security')}}
-      </el-menu-item>
-      <el-menu-item v-if="hasLicense" :index="'/clusters/detail/'+project+'/'+name+'/grade'">{{$t('cluster.detail.tag.grade')}}
-      </el-menu-item>
-    </el-menu>
-    <br />
-    <div class="detailClass">
-    <router-view></router-view>
+    <div v-loading="loading">
+      <el-menu class="menuClass" @select="search" router :default-active="$route.path" mode="horizontal">
+        <el-menu-item :index="'/clusters/detail/'+project+'/'+name+'/overview'">{{$t('cluster.detail.tag.overview')}}</el-menu-item>
+        <el-menu-item :index="'/clusters/detail/'+project+'/'+name+'/node'">{{$t('cluster.detail.tag.node')}}</el-menu-item>
+        <el-menu-item :index="'/clusters/detail/'+project+'/'+name+'/namespace'">{{$t('cluster.detail.tag.namespace')}}</el-menu-item>
+        <el-menu-item :index="'/clusters/detail/'+project+'/'+name+'/storage'">{{$t('cluster.detail.tag.storage')}}</el-menu-item>
+        <el-menu-item :index="'/clusters/detail/'+project+'/'+name+'/event'">{{$t('cluster.detail.tag.event')}}</el-menu-item>
+        <el-menu-item v-if="isPrometheusOn" :index="'/clusters/detail/'+project+'/'+name+'/monitor'">
+          {{$t('cluster.detail.tag.monitor')}}
+        </el-menu-item>
+        <el-menu-item v-if="isLoggingOn" :index="'/clusters/detail/'+project+'/'+name+'/logging'">{{$t('cluster.detail.tag.log')}}
+        </el-menu-item>
+        <el-menu-item v-if="isLokiOn" :index="'/clusters/detail/'+project+'/'+name+'/loki'">{{$t('cluster.detail.tag.log')}}
+        </el-menu-item>
+        <el-menu-item :index="'/clusters/detail/'+project+'/'+name+'/tool'">{{$t('cluster.detail.tag.tool')}}</el-menu-item>
+        <el-menu-item v-if="arch === 'amd64'" :index="'/clusters/detail/'+project+'/'+name+'/istio'">Istio</el-menu-item>
+        <el-menu-item :index="'/clusters/detail/'+project+'/'+name+'/backup'">{{$t('cluster.detail.tag.backup')}}</el-menu-item>
+        <el-menu-item v-if="hasLicense" :index="'/clusters/detail/'+project+'/'+name+'/f5'">F5</el-menu-item>
+        <el-menu-item :index="'/clusters/detail/'+project+'/'+name+'/security'">
+          {{$t('cluster.detail.tag.security')}}
+        </el-menu-item>
+        <el-menu-item v-if="hasLicense" :index="'/clusters/detail/'+project+'/'+name+'/grade'">{{$t('cluster.detail.tag.grade')}}
+        </el-menu-item>
+      </el-menu>
+      <br />
+      <div class="detailClass">
+        <router-view></router-view>
+      </div>
     </div>
   </layout-content>
 </template>
@@ -33,6 +35,7 @@
 <script>
 import LayoutContent from "@/components/layout/LayoutContent"
 import { listTool } from "@/api/cluster/tool"
+import { getClusterByName } from "@/api/cluster"
 
 export default {
   name: "ClusterDetail",
@@ -40,14 +43,27 @@ export default {
   components: { LayoutContent },
   data() {
     return {
-      isLokiOn: false,
-      isLoggingOn: false,
-      isPrometheusOn: false,
-      hasLicense: false,
+      isLokiOn: null,
+      isLoggingOn: null,
+      isPrometheusOn: null,
+      hasLicense: null,
+      arch: null,
+      loading: false,
     }
   },
   methods: {
     search() {
+      this.loading = true
+      getClusterByName(this.$route.params.name)
+        .then((data) => {
+          if (data.spec.architectures) {
+            this.arch = data.spec.architectures
+            this.loading = false
+          }
+        })
+        .catch(() => {
+          this.loading = false
+        })
       listTool(this.$route.params.name).then((data) => {
         data.forEach((item) => {
           if (item.name === "logging") {
