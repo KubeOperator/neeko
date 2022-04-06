5786d7b15cfa6ddbf97b8aaaf0fc10e64d168cff
wangzhengkun <zhengkun@fit2cloud.com>
2022-04-06 16:28:45 +0800

    feat: 增加模版配置创建

--- src/api/template-config.js
+++ src/api/template-config.js
@@ -2,7 +2,11 @@ import {post} from "@/plugins/request"
 
 const templateUrl = "/api/v1/templates"
 
-export function searchTemplates (page, size,condition) {
-  return post(`${templateUrl}/search?pageNum=${page}&pageSize=${size}`,condition)
+export function searchTemplates (page, size, condition) {
+  return post(`${templateUrl}/search?pageNum=${page}&pageSize=${size}`, condition)
+}
+
+export function createTemplate (data) {
+  return post(`${templateUrl}/create`, data)
 }
 
--- src/business/automatic/template-configs/operate/index.vue
+++ src/business/automatic/template-configs/operate/index.vue
@@ -2,7 +2,7 @@
   <layout-content :header="$t('commons.button.create')" :back-to="{name:'TemplateList'}">
     <el-row>
       <el-col :span="4"><br/></el-col>
-      <el-col :span="10">
+      <el-col :span="12">
         <div class="grid-content bg-purple-light">
           <el-form ref="form" :model="form" :rules="rules" label-width="200px" label-position="left">
             <el-form-item :label="$t('commons.table.name')" prop="name">
@@ -11,24 +11,27 @@
             </el-form-item>
             <el-form-item :label="$t('commons.table.type')" prop="type">
               <el-select v-model="form.type">
-                  <el-option
-                      v-for="(item,index) in types"
-                      :key="index"
-                      :label="item.name"
-                      :value="item.value">
-                  </el-option>
+                <el-option
+                        v-for="(item,index) in types"
+                        :key="index"
+                        :label="item.name"
+                        :value="item.value">
+                </el-option>
               </el-select>
             </el-form-item>
             <el-form-item :label="$t('automatic.template_config.template_name')" prop="config.name">
               <el-input v-model="form.config.name"></el-input>
             </el-form-item>
-            <el-form-item :label="$t('automatic.template_config.qcow2_path')" prop="config.qcow2_path" v-if="form.type === 'OpenStack'">
+            <el-form-item :label="$t('automatic.template_config.qcow2_path')" prop="config.qcow2_path"
+                          v-if="form.type === 'OpenStack'">
               <el-input v-model="form.config.qcow2_path"></el-input>
             </el-form-item>
-            <el-form-item :label="$t('automatic.template_config.vmdk_path')" prop="config.vmdk_path" v-if="form.type === 'vSphere'">
+            <el-form-item :label="$t('automatic.template_config.vmdk_path')" prop="config.vmdk_path"
+                          v-if="form.type === 'vSphere'">
               <el-input v-model="form.config.vmdk_path"></el-input>
             </el-form-item>
-            <el-form-item :label="$t('automatic.template_config.ovf_path')" prop="config.ovf_path" v-if="form.type === 'vSphere'">
+            <el-form-item :label="$t('automatic.template_config.ovf_path')" prop="config.ovf_path"
+                          v-if="form.type === 'vSphere'">
               <el-input v-model="form.config.ovf_path"></el-input>
             </el-form-item>
             <el-form-item>
@@ -49,6 +52,7 @@
 <script>
 import LayoutContent from "@/components/layout/LayoutContent"
 import Rule from "@/utils/rules"
+import {createTemplate} from "@/api/template-config"
 
 export default {
   name: "TemplateOperate",
@@ -72,7 +76,7 @@ export default {
       rules: {
         name: [Rule.LengthRule, Rule.NameRule],
         type: [Rule.RequiredRule],
-        config:{
+        config: {
           name: [Rule.RequiredRule],
           qcow2_path: [Rule.RequiredRule],
           vmdk_path: [Rule.RequiredRule],
@@ -85,8 +89,22 @@ export default {
     onCancel () {
       this.$router.push({ path: "/automatic/templates" })
     },
-    onSubmit () {
-
+    onSubmit (formName) {
+      this.$refs[formName].validate((valid) => {
+        if (!valid) {
+          return false
+        }
+        this.loading = true
+        createTemplate(this.form).then(() => {
+          this.$message({
+            type: "success",
+            message: this.$t("commons.msg.create_success")
+          })
+          this.$router.push({ name: "TemplateList" })
+        }).finally(() => {
+          this.loading = false
+        })
+      })
     }
   },
   created () {
