a3c39edf8a8cc9d16b361b4dbfd01f6b2e5b39ab
ssongliu <songlius11@163.com>
2022-07-26 15:53:05 +0800

    fix(message): 站内信错误显示格式化

--- src/business/app-layout/header-components/StationLetter.vue
+++ src/business/app-layout/header-components/StationLetter.vue
@@ -4,24 +4,20 @@
       <el-badge :value="count" :max="99" style="margin-top: 8px;" size="mini">
         <div style="margin-top: -12px;margin-right: 8px;cursor : pointer;">
           <i class="el-icon-message" style="color: #FA5D50;margin-right: 3px"></i>
-            <el-button style="color: #606266;font-size: 14px;" type="text" @click="openDrawer">{{$t('message.message_in_station')}}</el-button>
+          <el-button style="color: #606266;font-size: 14px;" type="text" @click="openDrawer">{{$t('message.message_in_station')}}</el-button>
         </div>
       </el-badge>
     </div>
 
-    <el-drawer :title="$t('message.message_in_station')" :visible.sync="drawer" direction="rtl" size="25%"
-               :modal="false">
+    <el-drawer :title="$t('message.message_in_station')" :visible.sync="drawer" direction="rtl" size="25%" :modal="false">
       <div class="notice-list" v-infinite-scroll="next" v-loading="letterLoading">
-        <div class="notice-item" v-for="(item, index) in messages" :key="index" @mouseover="hover = index"
-             @mouseleave="hover = null" @click="open(item)">
+        <div class="notice-item" v-for="(item, index) in messages" :key="index" @mouseover="hover = index" @mouseleave="hover = null" @click="open(item)">
           <div class="notice-title">
             <div class="text">
-              <svg v-if="item.readStatus === 'READ'" class="icon svg-icon" aria-hidden="true"
-                   :style="{ color: '#969696'}" size="5x">
+              <svg v-if="item.readStatus === 'READ'" class="icon svg-icon" aria-hidden="true" :style="{ color: '#969696'}" size="5x">
                 <use xlink:href="#iconyidu1"></use>
               </svg>
-              <svg v-if="item.readStatus === 'UNREAD'" class="icon svg-icon" aria-hidden="true"
-                   :style="{ color: '#0079b8' }">
+              <svg v-if="item.readStatus === 'UNREAD'" class="icon svg-icon" aria-hidden="true" :style="{ color: '#0079b8' }">
                 <use xlink:href="#iconweidu1"></use>
               </svg>
               {{ item.content.operator }}
@@ -55,9 +51,33 @@
           <el-descriptions-item :label="$t('commons.table.action')">{{ item.content.operator }}</el-descriptions-item>
           <el-descriptions-item :label="$t('commons.table.status')">{{ item.content.title }}</el-descriptions-item>
           <el-descriptions-item :label="$t('message.detail.detail')" v-if="item.content.errMsg">
-            {{ item.content.errMsg }}
+            <div v-if="item.itemMsg[0].type === 'unFormat'">
+              <div><span style="white-space: pre-wrap;">{{item.itemMsg[0].info | errorFormat }}</span></div>
+            </div>
+            <div v-else>
+              <div v-for="(msg, index) in item.itemMsg" :key="index">
+                <el-card :header="msg.name">
+                  <div v-if="msg.info.cmd">
+                    <div style="margin-top: 2px"><span style="font-weight: bold">cmd</span></div>
+                    <div><span style="white-space: pre-wrap;">{{msg.info.cmd }}</span></div>
+                  </div>
+                  <div v-if="msg.info.msg">
+                    <div style="margin-top: 2px"><span style="font-weight: bold">message</span></div>
+                    <div><span style="white-space: pre-wrap;">{{msg.info.msg }}</span></div>
+                  </div>
+                  <div v-if="msg.info.stderr">
+                    <div style="margin-top: 2px"><span style="font-weight: bold">stderr</span></div>
+                    <div><span style="white-space: pre-wrap;">{{msg.info.stderr }}</span></div>
+                  </div>
+                  <div v-if="msg.info.stdout">
+                    <div style="margin-top: 2px"><span style="font-weight: bold">stdout</span></div>
+                    <div><span style="white-space: pre-wrap;">{{msg.info.stdout }}</span></div>
+                  </div>
+                </el-card>
+              </div>
+            </div>
           </el-descriptions-item>
-          <el-descriptions-item :label="$t('message.detail.time')">{{ item.content.createdAt | datetimeFormat }}
+          <el-descriptions-item :label="$t('message.detail.time')">{{ item.content.createdAt }}
           </el-descriptions-item>
         </el-descriptions>
       </div>
@@ -70,13 +90,14 @@
 </template>
 
 <script>
-import {listUserMessages, readUserMessage} from "@/api/user-message"
+import { listUserMessages, readUserMessage } from "@/api/user-message"
+import { ansibleErrFormat } from "@/utils/format_ansible_err"
 
 export default {
   name: "StationLetter",
   components: {},
   props: {},
-  data () {
+  data() {
     return {
       count: 0,
       drawer: false,
@@ -91,110 +112,109 @@ export default {
       openDetail: false,
       item: {
         content: {},
-        msg: {}
+        msg: {},
       },
-      timer: {}
+      timer: {},
+      activeCollapse: 0,
     }
   },
   methods: {
-    openDrawer () {
+    openDrawer() {
       this.drawer = true
     },
-    load () {
+    load() {
       this.letterLoading = true
-      listUserMessages(this.paginationConfig.currentPage, this.paginationConfig.pageSize).then(res => {
+      listUserMessages(this.paginationConfig.currentPage, this.paginationConfig.pageSize)
+        .then((res) => {
+          this.messages = res.items
+          this.count = res.unread
+          this.paginationConfig.total = res.total
+        })
+        .finally(() => {
+          this.letterLoading = false
+        })
+    },
+    loadWithOutLoading() {
+      listUserMessages(this.paginationConfig.currentPage, this.paginationConfig.pageSize).then((res) => {
         this.messages = res.items
         this.count = res.unread
         this.paginationConfig.total = res.total
-      }).finally(() => {
-        this.letterLoading = false
       })
     },
-    loadWithOutLoading () {
-      listUserMessages(this.paginationConfig.currentPage, this.paginationConfig.pageSize).then(res => {
-        this.messages = res.items
-        this.count = res.unread
-        this.paginationConfig.total = res.total
-      })
-    },
-    markAsRead (item) {
+    markAsRead(item) {
       readUserMessage(item.id).finally(() => {
         this.count--
         item.readStatus = "READ"
         this.openDetail = false
       })
     },
-    open (item) {
+    open(item) {
       this.openDetail = true
       this.item = item
+      this.item.itemMsg = ansibleErrFormat(item.content.errMsg)
     },
-    next () {
+    next() {
       if (this.paginationConfig.pageSize >= this.paginationConfig.total) {
         return
       }
       this.paginationConfig.pageSize = this.paginationConfig.pageSize + 8
       this.load()
-    }
+    },
   },
-  created () {
+  created() {
     this.load()
     this.timer = setInterval(this.loadWithOutLoading, 60000)
   },
-  beforeDestroy () {
+  beforeDestroy() {
     clearInterval(this.timer)
-  }
+  },
 }
 </script>
 
 <style scoped lang="scss">
-  .el-divider--horizontal {
-    margin: 1px 0;
-  }
+.el-divider--horizontal {
+  margin: 1px 0;
+}
 
-  .notice-list {
-    overflow: auto;
-    height: 100%;
-    padding: 0 5px;
+.notice-list {
+  overflow: auto;
+  height: 100%;
+  padding: 0 5px;
 
-    .notice-item {
+  .notice-item {
+    padding: 12px 15px 8px;
+    border-bottom: 1px solid #f0f0f0;
+    cursor: pointer;
 
-      padding: 12px 15px 8px;
-      border-bottom: 1px solid #f0f0f0;
-      cursor: pointer;
-
-      &:hover {
-        background: #f5f7fa;
-      }
-
-      .notice-title {
-        display: flex;
-        align-items: flex-start;
-        justify-content: space-between;
-        color: #666666;
-
-        .text {
-          flex: 1;
-          width: 100%;
-          overflow: hidden;
-          white-space: nowrap;
-          text-overflow: ellipsis;
-        }
-
-        .read-button {
-          padding: 0;
-          margin-top: 10px;
-        }
-
-      }
-
-      .notice-content {
-        line-height: 25px;
-        padding: 5px 0;
-        font-weight: bold;
-      }
-
-
+    &:hover {
+      background: #f5f7fa;
     }
 
+    .notice-title {
+      display: flex;
+      align-items: flex-start;
+      justify-content: space-between;
+      color: #666666;
+
+      .text {
+        flex: 1;
+        width: 100%;
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+      }
+
+      .read-button {
+        padding: 0;
+        margin-top: 10px;
+      }
+    }
+
+    .notice-content {
+      line-height: 25px;
+      padding: 5px 0;
+      font-weight: bold;
+    }
   }
+}
 </style>
