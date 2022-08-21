8567f45a5e126d577ba80045560f53c307410406
ssongliu <songlius11@163.com>
2022-08-21 23:37:54 +0800

    fix: 处理导入的非 ko 集群

--- src/business/clusters/detail/index.vue
+++ src/business/clusters/detail/index.vue
@@ -6,13 +6,13 @@
         <el-menu-item :index="'/clusters/detail/'+project+'/'+name+'/node'">{{$t('cluster.detail.tag.node')}}</el-menu-item>
         <el-menu-item :index="'/clusters/detail/'+project+'/'+name+'/storage'">{{$t('cluster.detail.tag.storage')}}</el-menu-item>
         <el-menu-item :index="'/clusters/detail/'+project+'/'+name+'/backup'">{{$t('cluster.detail.backup.backup_recover')}}</el-menu-item>
-        <el-menu-item :index="'/clusters/detail/'+project+'/'+name+'/component'">{{$t('cluster.detail.tag.component')}}</el-menu-item>
+        <el-menu-item v-if="source !== 'external'" :index="'/clusters/detail/'+project+'/'+name+'/component'">{{$t('cluster.detail.tag.component')}}</el-menu-item>
         <el-menu-item :index="'/clusters/detail/'+project+'/'+name+'/tool'">{{$t('cluster.detail.tag.tool')}}</el-menu-item>
         <el-menu-item :index="'/clusters/detail/'+project+'/'+name+'/monitor'">{{$t('cluster.detail.tag.monitor')}}</el-menu-item>
         <el-menu-item :index="'/clusters/detail/'+project+'/'+name+'/log'">{{$t('cluster.detail.tag.log')}}</el-menu-item>
         <el-menu-item :index="'/clusters/detail/'+project+'/'+name+'/security'">{{$t('cluster.detail.tag.security')}}</el-menu-item>
         <el-menu-item :index="'/clusters/detail/'+project+'/'+name+'/grade'">{{$t('cluster.detail.tag.grade')}}</el-menu-item>
-        <el-menu-item :index="'/clusters/detail/'+project+'/'+name+'/task'">{{$t('cluster.detail.tag.task')}}</el-menu-item>
+        <el-menu-item v-if="source !== 'external'" :index="'/clusters/detail/'+project+'/'+name+'/task'">{{$t('cluster.detail.tag.task')}}</el-menu-item>
       </el-menu>
       <br />
       <div>
@@ -34,6 +34,7 @@ export default {
     return {
       hasLicense: null,
       arch: null,
+      source: "",
       loading: false,
       clusterName: "",
     }
@@ -45,6 +46,7 @@ export default {
         .then((data) => {
           if (data.architectures) {
             this.arch = data.architectures
+            this.source = data.source
             this.loading = false
           }
         })
